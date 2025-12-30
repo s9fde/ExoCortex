6dd9c863f3a84b4872ef6c0b295ebdfa64fad6b9
Tom <git@s9f.de>
2025-12-30 21:14:56 +0100

    Initial Commit

---
+++ ExoCortex/ExoCortexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExoCortexApp.swift
+//  ExoCortex
+//
+//  Created by Tom Schlenkhoff on 30.12.25.
+//
+
+import SwiftUI
+
+@main
+struct ExoCortexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
